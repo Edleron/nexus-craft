c8d0e8d1dd4eef5c576217488e3d595293f1528e
Edleron <Edleron@hotmail.com>
2025-07-18 00:20:53 +0300

    Add splash screen component with HTML, CSS, TypeScript, and configuration

---
+++ src/app/components/splash-screen/splash.config.ts
@@ -0,0 +1,6 @@
+// src/app/components/splash-screen/splash-screen.config.ts
+
+export const SPLASH_SCREEN_CONFIG = {
+  // Splash ekranının ekranda kalma süresi (milisaniye cinsinden)
+  duration: 2000,
+};
