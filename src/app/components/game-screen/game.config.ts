caffe4f712837284f808290ac66a3264aea0f0b1
Edleron <Edleron@hotmail.com>
2025-07-24 15:51:34 +0300

    refactor: Remove game screen component files and associated styles

--- src/app/components/game-screen/game.config.ts
+++
@@ -1,6 +0,0 @@
-// src/app/components/loading-screen/loading-screen.config.ts
-
-export const GAME_SCREEN_CONFIG = {
-  // Asset yüklemesini simüle etmek için kullanılacak sahte gecikme (ms)
-  fakeLoadingTime: 3000,
-};
