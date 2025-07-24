3d038160b217addca8476ad1c2ec407a5090e3eb
Edleron <Edleron@hotmail.com>
2025-07-24 16:19:51 +0300

    feat: Add TranslocoHttpLoader for dynamic translation loading and update app configuration for localization

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,40 @@
-import { ApplicationConfig, APP_INITIALIZER, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, APP_INITIALIZER, provideExperimentalZonelessChangeDetection, isDevMode } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@ngneat/transloco';
+import { TranslocoHttpLoader } from './transloco-loader';
 import { CreateService } from './services/create.service';
+import { environment } from '../environments/environment';
 
 // APP_INITIALIZER için bir factory fonksiyonu
 // Bu fonksiyon, InitializationService'i enjekte eder ve onun init metodunu döndürür.
-export function createAppFactory(initializationService: CreateService): () => Promise<void> {
-  return () => initializationService.init();
+export function createAppFactory(creatingService: CreateService): () => Promise<void> {
+  return () => creatingService.init();
 }
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
 
-    // APP_INITIALIZER'ı burada sağlıyoruz
+    // Transloco'nun çalışması için HttpClient gereklidir.
+    provideHttpClient(), 
+    
+    // Transloco'nun yeni provider tabanlı kurulumu
+    provideTransloco({
+      config: { 
+        availableLangs: ['en', 'tr'], // Bu dilleri transloco.config.js'den alabilirsiniz
+        defaultLang: 'tr',
+        // production modunda yeniden yüklemeyi devre dışı bırakarak performansı artırır
+        reRenderOnLangChange: true,
+        prodMode: environment.production,
+      },
+      loader: TranslocoHttpLoader
+    }),
+
     {
       provide: APP_INITIALIZER,
       useFactory: createAppFactory,
-      deps: [CreateService], // Factory'nin bağımlılıklarını belirtiyoruz
-      multi: true // Birden fazla initializer olabileceğini belirtir
+      deps: [CreateService],
+      multi: true
     }
   ]
 };
